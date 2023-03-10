900ef750a61527d4017726ca6519628bb877c974
nnistelrooij <47317906+nnistelrooij@users.noreply.github.com>
2023-03-10 09:14:23 +0100

    Update mandibles.py

--- jawfrac/data/datasets/mandibles.py
+++ jawfrac/data/datasets/mandibles.py
@@ -45,7 +45,7 @@ class MandibleSegDataset(VolumeDataset):
         intensities = np.asarray(img.dataobj)
 
         # convert 8-bit to 12-bit
-        if intensities.min() == 0 and intensities.max() == 255:
+        if intensities.min() == 0 and intensities.max() <= 255:
             center = intensities[intensities > 0].mean()
             intensities = (intensities - center) / 255 * 4095
 
